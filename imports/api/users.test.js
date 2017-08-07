63ef303be4b2d993ca5bfa876fc6e838b44f898c
Rye Crowen <ryecrowendesign@mac.com>
2017-08-06 22:38:57 -0400

    Testing of user authentication with Mocha and Expect

--- imports/api/users.test.js
+++ imports/api/users.test.js
@@ -1,27 +1,64 @@
-const add = (a, b) => {
-    if (typeof b !== 'number') {
-        return a + a
-    }
-    return a + b;
+import { Meteor } from 'meteor/meteor';
+import expect from 'expect';
+import { validateNewUser } from './users';
+
+if (Meteor.isServer) {
+    describe('users', function () {
+
+        it ('should allow vaild email addres', function () {
+            const testUser = {
+                emails: [
+                    {
+                        address: 'test@example.com'
+                    }
+                ]
+            };
+            const res = validateNewUser(testUser);
+
+            expect(res).toBe(true);
+        });
+
+        it ('should reject invalid email', function () {
+            const testUser = {
+                emails: [
+                    {
+                        address: 'test.com'
+                    }
+                ]
+            };
+
+            expect(() => {
+                validateNewUser(testUser);
+            }).toThrow();
+
+        });
+    });
 }
 
-describe('add', function () {
+// const add = (a, b) => {
+//     if (typeof b !== 'number') {
+//         return a + a
+//     }
+//     return a + b;
+// }
 
-    it('should add two numbers', function () {
-        const res = add(11, 9);
+// describe('add', function () {
 
-        if (res !== 20) {
-            throw new Error('Sum was not equal to expected value');
-        }
-    });
+//     it('should add two numbers', function () {
+//         const res = add(11, 9);
 
-    it('should double a single number', function () {
-        const res = add(8);
+//         expect(res).toBe(20);
 
-        if (res !== 16) {
-            throw new Error('Sum was not equal to expected value');
-        }
-    });
+//         // if (res !== 20) {
+//         //     throw new Error('Sum was not equal to expected value');
+//         // }
+//     });
 
-});
+//     it('should double a single number', function () {
+//         const res = add(8);
+
+//         expect(res).toBe(16);
+//     });
+
+// });
 
